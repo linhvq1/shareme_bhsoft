cac2832cb6b9c87785df462646cb0a85b4e277ec
linh16p <1851060148@e.tlu.edu.vn>
2022-06-26 19:48:39 +0700

    fix some bug

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import GoogleLogin from 'react-google-login'
 import jwt_decode from 'jwt-decode'
 import {useNavigate} from 'react-router-dom'
-import {FcGoogle} from 'react-icons/fc'
 import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 import { client } from '../client'
