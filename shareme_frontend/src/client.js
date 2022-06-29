9baea62f1e84a35b75f7818d632ae29ea67070f4
linh16p <1851060148@e.tlu.edu.vn>
2022-06-29 18:18:52 +0700

    Fix big bug remove useCdn -> usesCdn

--- shareme_frontend/src/client.js
+++ shareme_frontend/src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-06-25',
-    useCdn: true,
+    usesCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
 })
 
