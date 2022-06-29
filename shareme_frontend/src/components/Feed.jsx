c8655d87c24a5fbb4ca419b4476b1cbd61a44cca
HieuND <hieund.bhsoft@gmail.com>
2022-06-29 15:26:23 +0700

    fix delay

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,38 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 // mot loai tronh router
 // no tra ve 1 object cua params dong tu URL hien tai, cai ma no match voi route
-import {useParams} from 'react-router-dom'
-import {client} from '../client'
-import { searchQuery } from '../utils/data'
-import { feedQuery } from '../utils/data'
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import { searchQuery } from "../utils/data";
+import { feedQuery } from "../utils/data";
 
 //cai layout giup cho may cai anh no so le y, nhin dep mat lam :))
-import MasonryLayout from './MasonryLayout'
-import Spinner from './Spinner'
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 function Feed() {
-    const [loading, setLoading] = useState(false)
-    const {categoryId} = useParams()
-    const [pins, setPins] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState([]);
+  const query = categoryId ? searchQuery(categoryId) : feedQuery;
+  async function fetchData() {
+    const result = await client.fetch(query);
+    setPins(result);
+  }
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [categoryId, fetchData]);
 
-    useEffect(() => {
-      setLoading(true)
-      const query = categoryId? searchQuery(categoryId) : feedQuery
-        client.fetch(query)
-        .then((data)=>{
-            setPins(data)
-            setLoading(false)
-        })
-      
-    }, [categoryId])
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed!" />;
 
-    if(loading) return <Spinner message="We are adding new ideas to your feed!" />
-
-  return (
-    <div>
-        {pins && <MasonryLayout pins={pins}/>}
-    </div>
-  )
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 }
 
-export default Feed
+export default Feed;
--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -1,139 +1,155 @@
-import React, {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {v4 as uuidv4} from 'uuid'
-import { client, urlFor } from '../client'
-import {MdDownloadForOffline} from 'react-icons/md'
-import {AiTwotoneDelete} from 'react-icons/ai'
-import {BsFillArrowUpRightCircleFill} from 'react-icons/bs'
-import { fetchUser } from '../utils/fetchUser'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { client, urlFor } from "../client";
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import { fetchUser } from "../utils/fetchUser";
+import { feedQuery } from "../utils/data";
 
 // {postedBy, image, _id, destination, }
-function Pin({pin:{postedBy, image, _id, destination, save}}) {
+function Pin({ pin: { postedBy, image, _id, destination, save } }) {
+  const [postHovered, setPostHovered] = useState(false);
+  const navigate = useNavigate();
+  const user = fetchUser();
 
-    const [postHovered, setPostHovered] = useState(false)
-    const navigate = useNavigate()
-    const user = fetchUser()
+  const alreadySaved = !!save?.filter((item) => item.postedBy._id === user.sub)
+    ?.length;
 
-    const alreadySaved = !!(save?.filter((item)=>item.postedBy._id === user.sub))?.length
+  // neu nhu muon lay ra 1 trong [1,2,3] ->[1].length ->tuc la co so 1 va do dai tra ve la 1
+  // -> !1 -> false -> !false -> true co nghia la da luu
 
-    // neu nhu muon lay ra 1 trong [1,2,3] ->[1].length ->tuc la co so 1 va do dai tra ve la 1
-    // -> !1 -> false -> !false -> true co nghia la da luu 
-    
-    //nguoc lai muon lay ra 4 trong [1,2,3] ->[].length ->va mag tra ve k co so nap tuc la do dai tra ve la 0
-    // -> !0 -> true -> !true -> false tuc la chua luu cai nao
+  //nguoc lai muon lay ra 4 trong [1,2,3] ->[].length ->va mag tra ve k co so nap tuc la do dai tra ve la 0
+  // -> !0 -> true -> !true -> false tuc la chua luu cai nao
 
-    // muc dich cua cai nay la de luu lai state
-    const savePin = (id) => {
-
-        if(!alreadySaved){
-            
-            client.patch(id)
-            .setIfMissing({save: []})
-            .insert('after','save[-1]',[{
-                _key: uuidv4(),
-                userId: user.sub,
-                postedBy:{
-                    _type: 'postedBy',
-                    _ref: user.sub,
-                },
-            },])
-            .commit()
-            .then(()=>{
-                window.location.reload()
-            })
-        }
+  // muc dich cua cai nay la de luu lai state
+  const savePin = (id) => {
+    if (!alreadySaved) {
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user.sub,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user.sub,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          //   window.location.reload();
+        });
     }
+  };
 
-    const deletePin = (id)=>{
-        client
-         .delete(id)
-         .then(()=>{
-            window.location.reload()
-         })
-    }
+  const deletePin = (id) => {
+    client.delete(id).then(() => {
+      //   window.location.reload();
+    });
+  };
 
   return (
-    <div className='m-2'>
-        <div
-            onMouseEnter={()=>setPostHovered(true)}
-            onMouseLeave={()=>setPostHovered(false)}
-            onClick={()=> navigate(`/pin-detail/${_id}`)}
-            className = "relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
-        >
-            <img className='rounded-lg w-full' src={urlFor(image).width(250).url()} alt="user post"/>
-            {postHovered && (
-                <div
-                    className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pr-2 pb-2 z-50'
-                    style={{height: '100%'}}
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          className="rounded-lg w-full"
+          src={urlFor(image).width(250).url()}
+          alt="user post"
+        />
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pr-2 pb-2 z-50"
+            style={{ height: "100%" }}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  //Phương thức stopPropagation() của đối tượng event được sử dụng để ngăn không cho sự kiện lan toả lên các phần tử mẹ của phần tử mà ở đó diễn ra sự kiện.
+                  // binh thuong k co cai method nay thi khi nhan no se bay sag ben pindetail
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 flex justify-center items-center rounded-full text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
                 >
-                    <div className='flex items-center justify-between'>
-                        <div className='flex gap-2'>
-                            <a 
-                                href= {`${image?.asset?.url}?dl=`}
-                                download
-                                //Phương thức stopPropagation() của đối tượng event được sử dụng để ngăn không cho sự kiện lan toả lên các phần tử mẹ của phần tử mà ở đó diễn ra sự kiện.
-                                // binh thuong k co cai method nay thi khi nhan no se bay sag ben pindetail
-                                onClick={(e)=> e.stopPropagation()}  
-                                className="bg-white w-9 h-9 flex justify-center items-center rounded-full text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
-                            >
-                                <MdDownloadForOffline />
-                            </a>
-                        </div>
-                        {alreadySaved? (
-                            <button type='button' className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
-                                {save?.length} Saved
-                            </button>
-                        ):(
-                            <button type='button' className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
-                                onClick={(e)=>{
-                                    e.stopPropagation();
-                                    savePin(_id)
-                                }}
-                            >
-                                Save
-                            </button>
-                        )}
-                    </div>
-                    <div className='flex justify-between items-center gap-2 w-full'>
-                        {destination && (
-                            <a 
-                                href={destination}
-                                target="_blank"
-                                rel="noreferrer"
-                                className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'
-                            >
-                                <BsFillArrowUpRightCircleFill />
-                                {destination.length > 20? destination.slice(8,20):destination.slice(8)}
-                            </a>
-                        )}
-                        {postedBy?._id === user.sub && (
-                            <button
-                                type='button'
-                                onClick={(e) =>{
-                                    e.stopPropagation()
-                                    deletePin(_id)
-                                }}
-                                className="bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outline-none"
-                            >
-                                <AiTwotoneDelete />
-                            </button>
-                        )}
-                    </div>
-                </div>
-            )}
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    alert("Already Saved");
+                  }}
+                >
+                  {save?.length} Saved
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                >
+                  Save
+                </button>
+              )}
             </div>
-            <Link 
-                to={`user-profile/${postedBy?._id}`}
-                className="flex gap-2 mt-2 items-center"
-            >
-                <img className='w-8 h-8 rounded-full object-cover'
-                    src={postedBy?.image}
-                    alt="user-profile"
-                />
-                <p className='font-semibold capitalize'>{postedBy?.userName}</p>
-            </Link>
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                >
+                  <BsFillArrowUpRightCircleFill />
+                  {destination.length > 20
+                    ? destination.slice(8, 20)
+                    : destination.slice(8)}
+                </a>
+              )}
+              {postedBy?._id === user.sub && (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                  className="bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outline-none"
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+      <Link
+        to={`user-profile/${postedBy?._id}`}
+        className="flex gap-2 mt-2 items-center"
+      >
+        <img
+          className="w-8 h-8 rounded-full object-cover"
+          src={postedBy?.image}
+          alt="user-profile"
+        />
+        <p className="font-semibold capitalize">{postedBy?.userName}</p>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Pin
+export default Pin;
